45eea6f2d17352573d88086535de9d35a8c757b5
5xF8EqMxKgLLehcFU <evancheungseek@gmail.com>
2024-07-22 08:30:30 -0500

    Update day-1-ranges.cpp

--- day-1-ranges.cpp
+++ day-1-ranges.cpp
@@ -1,17 +1,25 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
-#include <string>
 #include <ranges>
+#include <algorithm> // for std::ranges::sort, std::ranges::find, std::ranges::for_each
 
 int main() {
-    std::vector<int> vec = { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+
+    std::vector<int> vec = { 10, 3, 6, 1, 7, 4, 9, 2, 8, 5 };
 
     std::ranges::sort(vec);
 
-    for (int i : vec) {
-        std::cout << i << " ";
+    auto it = std::ranges::find(vec, 7);
+    if (it != vec.end()) {
+        std::cout << "Found 7 at position: " << std::distance(vec.begin(), it) << "\n";
     }
+    else {
+        std::cout << "7 not found in the vector.\n";
+    }
+
+    std::ranges::for_each(vec, [](int i) {
+        std::cout << i * i << " ";
+        });
 
     return 0;
 }
